334c14a0425e7da47c06a5533d213a64f83a0038
Vivian la Farina <vivianlafarina@gmail.com>
2023-05-11 01:18:07 -0500

    :art:

--- src/pages/PokemonId.jsx
+++ src/pages/PokemonId.jsx
@@ -36,11 +36,67 @@ const getPercentStatBar = (stat_base) => {
                     </div>
                 </section>
 
+                {/* General Information*/}
+                <section>
+                    <div>
+                        <h3>#{pokemon?.id}</h3>
+                    </div>
+
+                    <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-2">
+                        <hr />
+                        <h2 className=" capitalize font-bold">{pokemon?.name}</h2>
+                    </div>
+
+                    <div className="flex justify-center  gap-4 text-center ">
+                        <div>
+                            <h5>Weigth</h5>
+                            <span className=" font-bold">{pokemon?.weight}</span>
+
+                        </div>
+                        <div>
+                        <h5>Heigth</h5>
+                            <span className=" font-bold">{pokemon?.height}</span>
+
+                        </div>
+                    </div>
+                    </section>
+
+                    <section>
+                        {/* types*/}
+                        <section className="text-center">
+                            <h3>Types</h3>
+                            <section className="grid grid-cols-2 gap-4 mt-4">
+                                {
+                                    pokemon?.types.map(type => <article 
+                                        className=" p-2 px-8 border-[1px]
+                                         border-gray-300 " key={type.type.name}>{type.type.name}</article> )
+                                }
+                            </section>
+
+
+                    </section>
+                        {/* abilities*/}
+                          <section className="text-center">
+                            <h3>Habilities</h3>
+                            <section className="grid grid-cols-2 gap-4 mt-4">
+                                {
+                                    pokemon?.types.map(type => <article 
+                                        className=" p-2 px-8 border-[1px]
+                                         border-gray-300 " key={type.type.name}>{type.type.name}</article> )
+                                }
+                            </section>
+
+                          </section>
+
+
+
+                </section>
+
 
                 {/*seccion de stats*/}
                 <section>
                   <h3>Stats</h3>
-                  <section>
+                  <section className=" grid-cols-2 gap-2">
                     {    
                       pokemon?.stats.map(stat => (
                         <article  key={stat.stat.name}>
